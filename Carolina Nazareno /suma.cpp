86500bc85e81826be657451e010c39f859bed75b
carolinaulice2594 <carolina20@ymail.com>
2022-03-13 08:12:13 +0100

    Update suma.cpp desde pc

--- Carolina Nazareno /suma.cpp
+++ Carolina Nazareno /suma.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
-using namespace std;
-int main() {
-float a,b,c ;
-cin>>a>>b;
-c=a+b ;
-cout<<c;
-return 0;
-}
+using namespace std;//estaba st y le coloque std para que funcione
+int main()
+{
+	float A,B,C;
+	cout<<"Ingreso los dos numero A B:";
+	cin>>A>>B;
+	C=A+B;
+	cout<<"El resultado es este: ";
+	cout<<C<<endl;
+	return 0;
