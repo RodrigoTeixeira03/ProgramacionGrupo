dc9abf5af2cc01d33ad1688e24fb550f6a9a83d1
carolinanazareno2594 <carolina20@ymail.com>
2022-05-02 21:39:30 -0500

    Codigos

---
+++ NazarenoCarolina/ACTIVIDAD-B2/NazarenoCarolina-PuntoVenta.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int c=0, P;
+
+float A=0, x, Tb, Piva, Pdsc, iva=0.12, desc=0.30, vt;
+
+cout<<"Ingrese la cantidad de productos: ";
+cin>>P;
+do{
+cout<<"Ingrese el valor del producto: ";
+cin>>x;
+c=c+1;
+A=A+x;
+Piva=A*iva;
+cout<<"El valor del iva de la compra es de: $"<<Piva<<endl;
+Pdsc=A*desc;
+cout<<"El valor del descuento de su compra es de: $"<<Pdsc<<endl;
+Tb=Piva+Pdsc;
+
+}while (c<P);
+
+if (Tb==A){
+cout<<"Su compra aplica para un descuento:"<<Tb<<endl;
+
+iva=Tb*Piva/100;
+
+desc=Tb*Pdsc/100;
+
+}else{
+	
+vt=A+Piva-Pdsc;
+
+cout<<"El valor total es de: $"<<vt<<endl;
+}
+
+return(0);
+}
