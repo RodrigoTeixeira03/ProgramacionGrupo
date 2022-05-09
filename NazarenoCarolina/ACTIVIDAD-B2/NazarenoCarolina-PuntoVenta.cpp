f2322cf9dc975c6c217e3d196660ee4d3027f184
carolinanazareno2594 <carolina20@ymail.com>
2022-05-08 21:49:46 -0500

    Program c++ and code

--- NazarenoCarolina/ACTIVIDAD-B2/NazarenoCarolina-PuntoVenta.cpp
+++ NazarenoCarolina/ACTIVIDAD-B2/NazarenoCarolina-PuntoVenta.cpp
@@ -1,39 +1,32 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
-int main ()
+
+int main()
 {
-int c=0, P;
+  float x,c=0,A=0,P,Vb,Viva,Vdes,Vf;
+  
+  cout<<"Ingrese el valor de P:"; 
+  cin>>P;
+  
+  do
+  {
 
-float A=0, x, Tb, Piva, Pdsc, iva=0.12, desc=0.30, vt;
+  cout<<"Ingrese el valor de x:"; 
+  cin>>x;
+  
+  c=c+1;
+  A=A+x;
 
-cout<<"Ingrese la cantidad de productos: ";
-cin>>P;
-do{
-cout<<"Ingrese el valor del producto: ";
-cin>>x;
-c=c+1;
-A=A+x;
-Piva=A*iva;
-cout<<"El valor del iva de la compra es de: $"<<Piva<<endl;
-Pdsc=A*desc;
-cout<<"El valor del descuento de su compra es de: $"<<Pdsc<<endl;
-Tb=Piva+Pdsc;
 
-}while (c<P);
-
-if (Tb==A){
-cout<<"Su compra aplica para un descuento:"<<Tb<<endl;
-
-iva=Tb*Piva/100;
-
-desc=Tb*Pdsc/100;
-
-}else{
-	
-vt=A+Piva-Pdsc;
-
-cout<<"El valor total es de: $"<<vt<<endl;
-}
-
-return(0);
+  }
+  
+  while(c<P);
+  Vb=A;
+  Viva=Vb*0.12;
+  Vdes=Vb*0.10;
+  Vf=Vb+Viva-Vdes;
+  
+  cout<<"valor final a pagar es de:";
+  cout<<Vf<<endl;
 }
